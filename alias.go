9f2b2bdbc27d4062debd19c98961e7411eaafec2
Konstantin Voykov <koykov0@gmail.com>
2022-01-05 22:49:49 +0200

    Code comment issue.

--- alias.go
+++ alias.go
@@ -1,24 +1,29 @@
 package mpsl
 
+// GetTLD returns TLD part of hostname and ICANN flag.
 func (db *DB) GetTLD(hostname []byte) (tld []byte, icann bool) {
 	_, _, tld, icann = db.Parse(hostname)
 	return
 }
 
+// GetEffectiveTLD returns only eTLD part of hostname.
 func (db *DB) GetEffectiveTLD(hostname []byte) (etld []byte) {
 	_, etld, _, _ = db.Parse(hostname)
 	return
 }
 
+// GetEffectiveTLDPlusOne return only eTLD1 part of hostname.
 func (db *DB) GetEffectiveTLDPlusOne(hostname []byte) (etld1 []byte) {
 	etld1, _, _, _ = db.Parse(hostname)
 	return
 }
 
+// GetETLD is a shorthand alias of GetEffectiveTLD.
 func (db *DB) GetETLD(hostname []byte) []byte {
 	return db.GetEffectiveTLD(hostname)
 }
 
+// GetETLD1 is a shorthand alias of GetEffectiveTLDPlusOne.
 func (db *DB) GetETLD1(hostname []byte) []byte {
 	return db.GetEffectiveTLDPlusOne(hostname)
 }
--- alias_str.go
+++ alias_str.go
@@ -2,6 +2,7 @@ package mpsl
 
 import "github.com/koykov/fastconv"
 
+// GetTLDStr returns TLD part of string hostname and ICANN flag.
 func (db *DB) GetTLDStr(hostname string) (tld string, icann bool) {
 	var btld []byte
 	_, _, btld, icann = db.Parse(fastconv.S2B(hostname))
@@ -9,22 +10,26 @@ func (db *DB) GetTLDStr(hostname string) (tld string, icann bool) {
 	return
 }
 
+// GetEffectiveTLDStr returns only eTLD part of string hostname.
 func (db *DB) GetEffectiveTLDStr(hostname string) (etld string) {
 	_, betld, _, _ := db.Parse(fastconv.S2B(hostname))
 	etld = fastconv.B2S(betld)
 	return
 }
 
+// GetEffectiveTLDPlusOneStr return only eTLD1 part of string hostname.
 func (db *DB) GetEffectiveTLDPlusOneStr(hostname string) (etld1 string) {
 	_, _, betld1, _ := db.Parse(fastconv.S2B(hostname))
 	etld1 = fastconv.B2S(betld1)
 	return
 }
 
+// GetETLDStr is a shorthand alias of GetEffectiveTLDStr.
 func (db *DB) GetETLDStr(hostname string) string {
 	return db.GetEffectiveTLDStr(hostname)
 }
 
+// GetETLD1Str is a shorthand alias of GetEffectiveTLDPlusOneStr.
 func (db *DB) GetETLD1Str(hostname string) string {
 	return db.GetEffectiveTLDPlusOneStr(hostname)
 }
